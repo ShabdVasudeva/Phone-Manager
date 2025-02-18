4d9acc83ff3a7f04b05a51ea9f6c51a8aa868d36
Shabd Vasudeva <127197800+ShabdVasudeva@users.noreply.github.com>
2025-02-18 19:20:52 +0530

    Update MainActivity.kt

--- app/src/main/java/apw/android/phonemanager/MainActivity.kt
+++ app/src/main/java/apw/android/phonemanager/MainActivity.kt
@@ -32,6 +32,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun mainScreen(){
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
