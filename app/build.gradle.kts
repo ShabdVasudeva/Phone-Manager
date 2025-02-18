a565c6e9e852f935ff0a9ab28367f3462aad7319
ShabdVasudeva <shabdvasudeva@gmail.com>
2025-02-18 08:10:13 +0530

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,68 @@
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "apw.android.phonemanager"
+    compileSdk = 35
+    
+    defaultConfig {
+        applicationId = "apw.android.phonemanager"
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+        
+        vectorDrawables { 
+            useSupportLibrary = true
+        }
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    buildFeatures {        
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.2"
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions.jvmTarget = "11"
+}
+
+dependencies {
+    implementation(platform("androidx.compose:compose-bom:2024.11.00"))
+    implementation("com.airbnb.android:lottie-compose:6.0.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.ui:ui")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("org.eclipse.jgit:org.eclipse.jgit:6.7.0.202309050840-r")
+    implementation("androidx.datastore:datastore-preferences:1.1.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+}
---
+++ app/src/main/java/apw/android/phonemanager/MainActivity.kt
@@ -0,0 +1,72 @@
+package apw.android.phonemanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.*
+import android.content.Context
+import androidx.compose.animation.*
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.gestures.*
+import android.view.accessibility.AccessibilityManager
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.*
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.draw.*
+import androidx.compose.ui.platform.*
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.input.nestedscroll.*
+import android.app.Activity
+import android.content.*
+import android.widget.*
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.*
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.*
+import apw.android.phonemanager.ui.theme.MyComposeApplicationTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyComposeApplicationTheme {
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background) {
+                    Main(LocalContext.current)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun Main(context : Context){
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text("Phone Manager",
+                        maxLines = 1, 
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+                actions = {
+                    
+                }
+            )
+        },
+        content = { innerPadding ->
+            
+        }
+    )
+}
