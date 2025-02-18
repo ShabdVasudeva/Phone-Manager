f28b2eee0a2cada8585136498d45bc65f758bb83
Shabd Vasudeva <127197800+ShabdVasudeva@users.noreply.github.com>
2025-02-18 19:15:40 +0530

    Update MainActivity.kt

--- app/src/main/java/apw/android/phonemanager/MainActivity.kt
+++ app/src/main/java/apw/android/phonemanager/MainActivity.kt
@@ -3,12 +3,17 @@ package apw.android.phonemanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.*
+import androidx.compose.material.icons.*
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.text.style.*
+import androidx.compose.ui.unit.*
+import androidx.compose.ui.input.nestedscroll.*
 import androidx.compose.ui.tooling.preview.Preview
 import apw.android.phonemanager.ui.theme.MyComposeApplicationTheme
 
@@ -20,7 +25,7 @@ class MainActivity : ComponentActivity() {
             MyComposeApplicationTheme {
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    mainScreen()
                 }
             }
         }
@@ -28,14 +33,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(text = "Hello $name!", modifier = modifier)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyComposeApplicationTheme {
-        Greeting("Android")
-    }
+fun mainScreen(){
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            title = Text(
+                "Phone Manager",
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            ),
+            scrollBehavior = scrollBehavior
+        },
+        content = { innerPadding ->
+            LazyColumn(
+                contentPadding = innerPadding,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                val list = (0..75).map { it.toString() }
+                items(count = list.size) {
+                    Text(
+                        text = list[it],
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+                    )
+                }
+            }
+        }
+    )
 }
