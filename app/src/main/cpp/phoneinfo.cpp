da2f8277625014ef1f2a6f586cd5f3590a8dfb46
Shabd Vasudeva <127197800+ShabdVasudeva@users.noreply.github.com>
2025-03-03 23:01:53 +0530

    ab²

--- app/src/main/cpp/phoneinfo.cpp
+++ app/src/main/cpp/phoneinfo.cpp
@@ -1,6 +1,5 @@
 #include <jni.h>
-
 extern "C" JNIEXPORT jstring JNICALL
 Java_apw_android_phonemanager_DeviceInfo_getDeviceName(JNIEnv *env, jobject thiz){
-  return "Oppo";
+  return env->NewStringUTF("Oppo");
 }
