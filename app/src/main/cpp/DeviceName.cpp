82d3d1af54074ea88ac864bad5fb9dcd75700a88
Shabd Vasudeva <127197800+ShabdVasudeva@users.noreply.github.com>
2025-03-06 11:31:06 +0530

    commit

--- app/src/main/cpp/DeviceName.cpp
+++ app/src/main/cpp/DeviceName.cpp
@@ -1,13 +1,11 @@
-#include <fstream>
+#include <sys/system_properties.h>
 #include <string>
 #include "DeviceName.h"
 
 std::string getDeviceName(){
-    std::ifstream file("/system/build.prop");
-    std::string line;
-    if (file.is_open() && std::getLine(file, line))
-    {
-        return line;
+    char name[PROP_VALUE_MAX +1] = {0};
+    if(__system_property_get("ro.product.model", name) > 0){
+        return std::string(name);
     }
     return "Unknown device";
 }
