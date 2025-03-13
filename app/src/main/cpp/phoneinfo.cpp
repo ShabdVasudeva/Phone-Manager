b97d8b4f7f071e36605d6abef6bb9f06beae5e30
Shabd Vasudeva <127197800+ShabdVasudeva@users.noreply.github.com>
2025-03-13 21:11:45 +0530

    hotfix

--- app/src/main/cpp/phoneinfo.cpp
+++ app/src/main/cpp/phoneinfo.cpp
@@ -4,7 +4,3 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_apw_android_phonemanager_DeviceInfo_getDeviceName(JNIEnv *env, jobject thiz){
   return env->NewStringUTF(getDeviceName().c_str());
 }
-extern "C" JNIEXPORT jstring JNICALL
-Java_apw_android_phonemanager_DeviceInfo_getCPUName(JNIEnv *env, jobject thiz){
-  return env->NewStringUTF(getCPUName().c_str());
-}
--- app/src/main/java/apw/android/phonemanager/DeviceInfo.kt
+++ app/src/main/java/apw/android/phonemanager/DeviceInfo.kt
@@ -5,5 +5,4 @@ object DeviceInfo{
         System.loadLibrary("systemutils")
     }
     external fun getDeviceName(): String
-    external fun getCPUName(): String
 }
