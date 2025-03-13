a89112fe2388eddf6ce00fe8a37f15436cf20e5b
Shabd Vasudeva <127197800+ShabdVasudeva@users.noreply.github.com>
2025-03-13 21:24:00 +0530

    fix

--- app/src/main/cpp/CPUInfo.h
+++ app/src/main/cpp/CPUInfo.h
@@ -4,9 +4,9 @@
 #include <jni.h>
 
 extern "C" {
-    JNIEXPORT jstring JNICALL Java_com_example_yourapp_CPU_getCPUDetails(JNIEnv *, jobject);
-    JNIEXPORT jstring JNICALL Java_com_example_yourapp_CPU_getCPUName(JNIEnv *, jobject);
-    JNIEXPORT jint JNICALL Java_com_example_yourapp_CPU_getNumCores(JNIEnv *, jobject);
+    JNIEXPORT jstring JNICALL Java_apw_android_phonemanager_CPU_getCPUDetails(JNIEnv *, jobject);
+    JNIEXPORT jstring JNICALL Java_apw_android_phonemanager_CPU_getCPUName(JNIEnv *, jobject);
+    JNIEXPORT jint JNICALL Java_apw_android_phonemanager_CPU_getNumCores(JNIEnv *, jobject);
 }
 
 #endif
