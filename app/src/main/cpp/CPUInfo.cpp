3c418ede70ac71abf84b949a2c58221ea42a6e9d
Shabd Vasudeva <127197800+ShabdVasudeva@users.noreply.github.com>
2025-03-15 13:57:45 +0530

    fix cpu name

--- app/src/main/cpp/CPUInfo.cpp
+++ app/src/main/cpp/CPUInfo.cpp
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <iostream>
 #include <map>
+#include <sys/utsname.h>
 
 std::string getCpuInfoField(const std::string& path) {
     std::ifstream file(path);
@@ -16,9 +17,21 @@ std::string getCpuInfoField(const std::string& path) {
     return "Unknown";
 }
 
+std::string getCPUName(){
+    std::ifstream file("/proc/cpuinfo");
+    std::string line;
+    while(syd::getline(file, line)){
+        if(line.find("Hardware") != std::string::npos || line.find("model name") != std::string::npos){
+            return line.substr(line.find(":")+2);
+        }
+    }
+    return "Null";
+}
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_apw_android_phonemanager_CPU_getCPUName(JNIEnv *env, jobject thiz) {
-    return env->NewStringUTF(getCpuInfoField("/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq").c_str());
+    std::string name = getCPUName();
+    return env->NewStringUTF(name.c_str());
 }
 
 extern "C" JNIEXPORT jint JNICALL
