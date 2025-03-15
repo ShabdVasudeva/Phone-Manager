4e4fe1b44d1da0c44aa29541e3c121a233d0354e
Shabd Vasudeva <127197800+ShabdVasudeva@users.noreply.github.com>
2025-03-15 13:40:45 +0530

    hotfix

--- app/src/main/cpp/CPUInfo.cpp
+++ app/src/main/cpp/CPUInfo.cpp
@@ -17,16 +17,16 @@ std::string getCpuInfoField(const std::string& path) {
 }
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_apw_android_phonemanager_CPU_Companion_getCPUName(JNIEnv *env, jobject thiz) {
+Java_apw_android_phonemanager_CPU_getCPUName(JNIEnv *env, jobject thiz) {
     return env->NewStringUTF(getCpuInfoField("/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq").c_str());
 }
 
 extern "C" JNIEXPORT jint JNICALL
-Java_apw_android_phonemanager_CPU_Companion_getCPUCores(JNIEnv *env, jobject thiz) {
+Java_apw_android_phonemanager_CPU_getCPUCores(JNIEnv *env, jobject thiz) {
     return sysconf(_SC_NPROCESSORS_ONLN);
 }
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_apw_android_phonemanager_CPU_Companion_getCPUArch(JNIEnv *env, jobject thiz) {
+Java_apw_android_phonemanager_CPU_getCPUArch(JNIEnv *env, jobject thiz) {
     return env->NewStringUTF(getCpuInfoField("/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq").c_str());
 }
--- app/src/main/java/apw/android/phonemanager/CPU.kt
+++ app/src/main/java/apw/android/phonemanager/CPU.kt
@@ -2,16 +2,14 @@ package apw.android.phonemanager;
 
 import android.os.Build
 
-class CPU{
-    companion object{
-        init{
-            System.loadLibrary("systemutils")
-        }
-        external fun getCPUName(): String
-        external fun getCPUCores(): Int
-        external fun getCPUArch(): String
-        fun getCPUAbi(): String{
-            return Build.SUPPORTED_ABIS.joinToString(", ")
-        }
+object CPU{
+    init{
+        System.loadLibrary("systemutils")
+    }
+    external fun getCPUName(): String
+    external fun getCPUCores(): Int
+    external fun getCPUArch(): String
+    fun getCPUAbi(): String{
+        return Build.SUPPORTED_ABIS.joinToString(", ")
     }
 }
